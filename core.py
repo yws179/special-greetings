85ad149418edd6345504d6f18658a968b851729c
yws <yws179@gmail.com>
2018-12-22 11:13:50 +0800

    dev

--- core.py
+++ core.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import itchat
+
 import factory
+from logger import logger
 
 
 def is_self():
@@ -10,11 +12,11 @@ def is_self():
 
 @itchat.msg_register([itchat.content.TEXT], True, False, False)
 def receive_msg(msg):
-    print(msg)
+    logger.debug(msg)
     user_id = msg['FromUserName']
     user_name = msg['User']['NickName']
     head_img = itchat.get_head_img(user_id)
-    file = open("./img/%s.jpg" % user_name, 'wb')
+    file = open("./images/%s.jpg" % user_name, 'wb')
     file.write(head_img)
     file.close()
     factory.create_card(user_name, 'christmas.jpg')
---
+++ logger.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+def get_logger():
+    """
+    创建日志实例
+    """
+    logger = logging.getLogger('logger')
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+
+    # fh = logging.FileHandler('./logs/sg.log')
+    # fh.setFormatter(formatter)
+    # fh.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.DEBUG)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
+
+
+logger = get_logger()
